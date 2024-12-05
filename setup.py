ad988ecdeb61b3c5bf7b80250f2eb42e1b93f913
elijah <ciriaco.elijah@gmail.com>
2024-12-05 08:21:51 +0800

    fix: changed to Win32GUI

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ script_path = os.path.join("src", "main.py")
 executables = [
     Executable(
         script_path,
-        base="Win64GUI",
+        base="Win32GUI",
         target_name="Billing.exe",
         icon=os.path.join("resources", "favicon.ico")
     )
